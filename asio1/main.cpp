919a8642d86498ad293d81ef2f67e5c7178730f1
ymao <yongjun.mao@commscope.com>
2022-03-19 10:57:37 +0800

    add threadpool

--- asio1/main.cpp
+++ asio1/main.cpp
@@ -2,21 +2,31 @@
 #include <boost/asio.hpp>
 #include <boost/regex.hpp>
 
+using boost::asio::ip::tcp;
+boost::asio::awaitable<void> echo(boost::asio::io_context &executor)
+{
+  try
+  {
+    tcp::socket socket(executor);
+    co_await socket.async_connect({tcp::v4(), 55555}, boost::asio::use_awaitable); // 异步执行连接服务器
+
+    char data[1024];
+    for (;;)
+    {
+      std::size_t n = co_await socket.async_read_some(boost::asio::buffer(data), boost::asio::use_awaitable);
+      co_await boost::asio::async_write(socket, boost::asio::buffer(data, n), boost::asio::use_awaitable);
+    }
+  }
+  catch (std::exception &e)
+  {
+    std::printf("echo Exception: %s\n", e.what());
+  }
+}
+
 int main()
 {
-    boost::regex reg{R"(\d{3})"};
-    boost::cmatch mat;
-    if(boost::regex_search("2sieu12345ss", mat, reg))
-    {
-        std::cout << mat.str() << std::endl;
-    }
-
-    boost::asio::io_context io;
-    boost::asio::steady_timer t(io, std::chrono::seconds(5));
-    t.async_wait([](auto err){
-        std::cout << "Hello, ASIO!" << std::endl;
-    });
-    
-    io.run();
-    return 0;
+  boost::asio::io_context executor(1);
+  boost::asio::co_spawn(executor, echo(executor), boost::asio::detached);
+  executor.run();
+  return 0;
 }
